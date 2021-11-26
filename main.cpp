49eca3806ab88ba459527183d8d167345531f37f
Rolands Cīrulis <rc2816@gymn.local>
2021-11-26 10:03:10 +0200

    Trešā augšuplāde, izmaiņas nosacījumos

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@ int main()
     int a, b, c, A, B, i, j
     ;
     int mas[A][B];
-    cout << "Ievadiet 2 skaitlus robezas 1-10 " << endl;
+    cout << "Ievadiet 2 skaitlus robezas 1-10! " << endl;
     cin >> a;
     cin >> b;
-    cout << " Ievadiet skaitli robezas 2-10 " << endl;
+    cout << " Ievadiet skaitli robezas 2-10! " << endl;
     cin >> c;
     A=a;
     B=b;
-    if((a<11)&&(a>0)&&(b>0)&&(b<11)&&(c>1)){
+
+
+
+
+
+      if((a<11)&&(a>0)&&(b>0)&&(b<11)&&(c>1)){
           for (i=0; i<a; i++){
    for (j=0; j<b; j++){
       mas[A][B]=rand()%c;
@@ -27,10 +32,11 @@ int main()
    }
    cout << "\n";
 }
-system("pause");
     } else {
-    cout << " Jus ievadijat nederigu skaitli " << endl;
+    cout << " Jus ievadijat nederigu skaitli!!! " << endl;
     }
-
-    return 0;
+return 0;
 }
+
+
+
