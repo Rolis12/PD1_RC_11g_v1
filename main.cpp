cb087748a077ce0b7c5d097cbbc1ce413571f5e0
Rolands Cīrulis <rc2816@gymn.local>
2021-11-26 09:29:55 +0200

    Otrā augšuplāde, pārbauda ievadītās vērtības

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 using namespace std;
+int ddm(int pk, int ok, int pv);
 
 int main()
 {
@@ -11,15 +12,15 @@ int main()
     int a, b, c, A, B, i, j
     ;
     int mas[A][B];
-    cout << "Ievadiet 3 skaitlus " << endl;
+    cout << "Ievadiet 2 skaitlus robezas 1-10 " << endl;
     cin >> a;
     cin >> b;
+    cout << " Ievadiet skaitli robezas 2-10 " << endl;
     cin >> c;
     A=a;
     B=b;
-
-
-   for (i=0; i<a; i++){
+    if((a<11)&&(a>0)&&(b>0)&&(b<11)&&(c>1)){
+          for (i=0; i<a; i++){
    for (j=0; j<b; j++){
       mas[A][B]=rand()%c;
       cout << mas[A][B] << " ";
@@ -27,5 +28,9 @@ int main()
    cout << "\n";
 }
 system("pause");
+    } else {
+    cout << " Jus ievadijat nederigu skaitli " << endl;
+    }
+
     return 0;
 }
