8a0ff168a6f2514018d6cb8ba6a9710d0c5a8ccc
Rolands Cīrulis <rc2816@gymn.local>
2021-12-03 09:07:08 +0200

    Ceturtā augšuplāde (sava funkcija)

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@
 #include<stdio.h>
 using namespace std;
 int ddm(int pk, int ok, int pv);
-
+int gadijuma_sk(int c);
 int main()
 {
     //Rolands Cîrulis 11g
     srand(time(0));
-    int a, b, c, A, B, i, j
-    ;
-    int mas[A][B];
+    int a, b, c, A, B, i, j, sum;
+
     cout << "Ievadiet 2 skaitlus robezas 1-10! " << endl;
     cin >> a;
     cin >> b;
@@ -22,20 +21,29 @@ int main()
 
 
 
-
+    int mas[A][B];
 
       if((a<11)&&(a>0)&&(b>0)&&(b<11)&&(c>1)){
           for (i=0; i<a; i++){
    for (j=0; j<b; j++){
-      mas[A][B]=rand()%c;
+      mas[A][B]=gadijuma_sk(c);
+      sum=sum+mas[A][B];
       cout << mas[A][B] << " ";
    }
    cout << "\n";
-}
+
+} cout << "Masiva elementu summa ir: " << sum << endl;
     } else {
     cout << " Jus ievadijat nederigu skaitli!!! " << endl;
     }
+
 return 0;
+}
+
+
+int gadijuma_sk(int c)
+{
+    return rand()%c;
 }
 
 
